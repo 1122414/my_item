cb43c8393542d31453a272163741bfed48c69731
1122414 <758370266@qq.com>
2025-02-20 13:06:41 +0800

    new file:   "learn/Pygame/01-\346\270\270\346\210\217\346\234\200\345\260\217\347\263\273\347\273\237.py"

---
+++ learn/Pygame/01-游戏最小系统.py
@@ -0,0 +1,22 @@
+import pygame
+
+# 1.初始化
+pygame.init()
+
+# 2.创建游戏窗口
+# set_mode(大小)
+pygame.display.set_mode((400, 600))
+# set_caption("My Game")设值游戏标题
+pygame.display.set_caption("My Game")
+# 设置窗口背景
+
+# 3.让游戏保持一直运行
+# game loop(游戏循环)
+while True:
+  # 4.检测事件
+  for event in pygame.event.get():
+    # print(event)
+    if event.type == pygame.QUIT:
+      # 退出游戏
+      # pygame.quit()
+      exit()
